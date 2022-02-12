d45b54d6e0964925f69809affaab72aec684b525
kaitmay <kait.brown23@gmail.com>
2022-02-11 19:54:19 -0500

    Added bootstrap

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 console.log("your index.js file is loaded correctly");
 
+
+$('.carousel').carousel({
+  interval: 200
+});
+
+$('#myCarousel').on('slide.bs.carousel', function () {
+  // do something...
+})
+
 $("button").click(function(){
   $("p").fadeIn();
 });
+
+$('.collapse').collapse()
+
